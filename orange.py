a5f1bb5b83dccc27e80b5288872479bad90e417a
James Ridgway <james@jamesridgway.co.uk>
2020-08-31 13:51:54 +0100

    Existing scripts

---
+++ orange.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# -------------------------------------------------------------------------------------------------
+# Basic script to turning off the lights.
+# -------------------------------------------------------------------------------------------------
+
+from rpi_ws281x import *
+
+# LED strip configuration:
+LED_COUNT      = 159     # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+# Main program logic follows:
+if __name__ == '__main__':
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Initialize the library (must be called once before other functions).
+    strip.begin()
+
+    brightness = 255
+    strip.setBrightness(brightness)
+    for i in range(0, strip.numPixels()):
+        strip.setPixelColor(i, Color(255, 64, 0))
+        strip.show()
+
