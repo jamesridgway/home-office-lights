5dc1aa35ae575fa903cf4b07f328e9fa1a4583c0
James Ridgway <james@jamesridgway.co.uk>
2020-08-31 14:12:05 +0100

    Tweak

--- strip_manager.py
+++ strip_manager.py
@@ -12,24 +12,21 @@ class StripManager:
         led_brightness = 255  # Set to 0 for darkest and 255 for brightest
         led_invert = False  # True to invert the signal (when using NPN transistor level shift)
         led_channel = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
-        return StripManager(led_count, led_pin, led_freq_hz, led_dma, led_brightness, led_invert, led_channel)
+        return StripManager(led_count, led_pin, led_freq_hz, led_dma, led_invert, led_brightness, led_channel)
 
-    def __init__(self, led_count, led_pin, led_freq_hz, led_dma, led_brightness, led_invert, led_channel):
-        self.strip = Adafruit_NeoPixel(led_count, led_pin, led_freq_hz, led_dma, led_brightness, led_invert,
+    def __init__(self, led_count, led_pin, led_freq_hz, led_dma, led_invert, led_brightness, led_channel):
+        self.strip = Adafruit_NeoPixel(led_count, led_pin, led_freq_hz, led_dma, led_invert, led_brightness,
                                        led_channel)
+        self.strip.begin()
 
     def solid_color(self, r, g, b):
-        self.strip.begin()
         self.strip.setBrightness(255)
         for i in range(0, self.strip.numPixels()):
             self.strip.setPixelColor(i, Color(r, g, b))
             self.strip.show()
 
     def clear(self):
-        self.strip.begin()
-        for i in range(0, self.strip.numPixels()):
-            self.strip.setPixelColor(i, Color(0, 0, 0))
-            self.strip.show()
+        self.solid_color(0, 0, 0)
 
     def orange(self):
         self.solid_color(255, 64, 0)
