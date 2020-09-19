5baa7a761867e856a552ac4d3f0238ce91bbbfa4
James Ridgway <james@jamesridgway.co.uk>
2020-09-19 14:38:35 +0100

    Service refinements

--- install-office-lights.sh
+++ install-office-lights.sh
@@ -8,8 +8,6 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 echo "Service config"
-sudo mkdir -p /var/run/redis
-sudo chown -R redis /var/run/redis
 cp ./home-office-lights/home-office-lights /etc/init.d/home-office-lights
 sudo chmod +x /etc/init.d/home-office-lights
 sudo update-rc.d home-office-lights defaults
